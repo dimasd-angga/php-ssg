7149387294e4106f880e980622a6de729b6a7b50
Dimas D. Angga <email.dimasdarfiangga@gmail.com>
2024-09-04 10:30:00 +0700

    feat: add CLI entry point with argv parsing

---
+++ src/Cli.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpSsg;
+
+class Cli
+{
+    private const VERSION = '0.1.0';
+
+    public function run(array $argv): int
+    {
+        $command = $argv[1] ?? 'help';
+        $args = array_slice($argv, 2);
+
+        switch ($command) {
+            case '--version':
+            case '-v':
+                $this->out('php-ssg ' . self::VERSION);
+                return 0;
+
+            case 'help':
+            case '--help':
+            case '-h':
+                $this->printHelp();
+                return 0;
+
+            default:
+                $this->err("Unknown command: {$command}");
+                $this->printHelp();
+                return 1;
+        }
+    }
+
+    private function printHelp(): void
+    {
+        $this->out("php-ssg " . self::VERSION);
+        $this->out("");
+        $this->out("Usage: php-ssg <command> [options]");
+        $this->out("");
+        $this->out("Commands:");
+        $this->out("  help              Show this help");
+        $this->out("  --version         Show version");
+    }
+
+    private function out(string $msg): void
+    {
+        fwrite(STDOUT, $msg . PHP_EOL);
+    }
+
+    private function err(string $msg): void
+    {
+        fwrite(STDERR, $msg . PHP_EOL);
+    }
+}
